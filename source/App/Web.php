2c4988fa2661ab00249b90f26fc704415422dc41
guilopes2020 <guilopesdev@gmail.com>
2023-05-02 09:47:28 -0300

    feat:

--- source/App/Web.php
+++ source/App/Web.php
@@ -2,8 +2,9 @@
 
 namespace Source\App;
 
-use Source\Core\Controller;
 use stdClass;
+use Source\Support\Pager;
+use Source\Core\Controller;
 
 /**
  * Web Controller
@@ -26,7 +27,7 @@ class Web extends Controller
      */
     public function home(): void
     {
-        $head = $this->seo->render(CONF_SITE_NAME . " - " . CONF_SITE_TITLE, CONF_SITE_DESC, url(), url("/assets/images/share.jpg"));
+        $head = $this->seo->render(CONF_SITE_NAME . " - " . CONF_SITE_TITLE, CONF_SITE_DESC, url(), theme("/assets/images/share.jpg"));
 
         echo $this->view->render("home", [
             'head'  => $head,
@@ -41,12 +42,44 @@ class Web extends Controller
      */
     public function about(): void
     {
-        $head = $this->seo->render("Descubra o " . CONF_SITE_NAME . " - " . CONF_SITE_DESC, CONF_SITE_DESC, url("/sobre"), url("/assets/images/share.jpg"));
+        $head = $this->seo->render("Descubra o " . CONF_SITE_NAME . " - " . CONF_SITE_DESC, CONF_SITE_DESC, url("/sobre"), theme("/assets/images/share.jpg"));
 
         echo $this->view->render("about", [
             'head'  => $head,
             'video' => 'IN5SyMJRmGY',
         ]);
+    }
+
+    /**
+     * Blog Method
+     *
+     * @param array|null $data
+     * @return void
+     */
+    public function blog(?array $data): void
+    {
+        $head = $this->seo->render("Blog - " . CONF_SITE_NAME, "Confira em nosso blog dicas e casadas de como controlar melhor suas contas. Vamos tomar um café?", url("/blog"), theme("/assets/images/share.jpg"));
+
+        $pager = new Pager(url('/blog/page/'));
+        $pager->pager(100, 10, ($data['page'] ?? 1));
+
+        echo $this->view->render("blog", [
+            'head'      => $head,
+            'paginator' => $pager->render(),
+        ]);
+    }
+
+    public function blogPost(array $data)
+    {
+        $postName = $data['post_name'];
+
+        $head = $this->seo->render("POST_NAME - " . CONF_SITE_NAME, "POST_HEADLINE", url("/blog/{$postName}"), theme("/assets/images/share.jpg"));
+
+        echo $this->view->render("blog-post", [
+            'head' => $head,
+            'data' => $this->seo->data(),
+        ]);
+
     }
 
     /**
@@ -56,7 +89,7 @@ class Web extends Controller
      */
     public function terms(): void
     {
-        $head = $this->seo->render(CONF_SITE_NAME . " - Termos de uso", CONF_SITE_DESC, url("/termos"), url("/assets/images/share.jpg"));
+        $head = $this->seo->render(CONF_SITE_NAME . " - Termos de uso", CONF_SITE_DESC, url("/termos"), theme("/assets/images/share.jpg"));
 
         echo $this->view->render("terms", [
             'head'  => $head,
@@ -79,7 +112,7 @@ class Web extends Controller
         $error->linkTitle = "continue navegando";
         $error->link = url_back();
 
-        $head = $this->seo->render("{$error->code} | {$error->title}", $error->message, url_back("/ops/{$error->code}"), url("/assets/images/share.jpg"), false);
+        $head = $this->seo->render("{$error->code} | {$error->title}", $error->message, url_back("/ops/{$error->code}"), theme("/assets/images/share.jpg"), false);
 
         echo $this->view->render("error", [
             'head'  => $head,
